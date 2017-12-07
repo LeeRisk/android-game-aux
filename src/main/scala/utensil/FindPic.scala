8ed07062b8b7d34cd407558246a7a79af7a9f646
yujieshui <a1178615156@gmail.com>
2017-12-07 13:42:54 +0800

    [add find pic]   find by edge in scala

--- src/main/scala/utensil/FindPic.scala
+++ src/main/scala/utensil/FindPic.scala
@@ -4,11 +4,16 @@ import models.{GoalImage, OriginalImage, Point}
 import utensil.PythonScript.findPic
 
 
-case class FindPic(original: OriginalImage, goal: GoalImage, threshold: Double = 0.95) {
+case class FindPic(original: OriginalImage,
+                   goal: GoalImage,
+                   threshold: Double,
+                   patten: FindPic.Patten.Value = FindPic.Patten.Default) {
   lazy val (similarity, topLeftPoint) = {
-    val (max, x, y) = findPic(
-      original.name.replaceAll("\\\\", "/"),
-      goal.name.replaceAll("\\\\", "/"))
+    val (max, x, y) = {
+      val originalName = original.name.replaceAll("\\\\", "/")
+      val goalName = goal.name.replaceAll("\\\\", "/")
+      findPic(originalName, goalName, patten.toString)
+    }
     max -> Point(x, y)
   }
 
@@ -19,3 +24,12 @@ case class FindPic(original: OriginalImage, goal: GoalImage, threshold: Double =
   def noFind: Boolean = !isFind
 }
 
+object FindPic {
+
+  object Patten extends Enumeration {
+    val Default = Value("default")
+    val edge    = Value("edge")
+  }
+
+
+}
--- src/main/scala/utensil/PythonScript.scala
+++ src/main/scala/utensil/PythonScript.scala
@@ -10,12 +10,13 @@ import jep.Jep
 object PythonScript {
   var libpath = System.getProperty("java.library.path")
   val userDir = System.getProperty("user.dir")
-  libpath =  s"${userDir}\\python-script;${libpath}"
+  libpath = s"${userDir}\\python-script;${libpath}"
   System.setProperty("java.library.path", libpath)
   println(System.getProperty("java.library.path"))
   implicit val exec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(1))
-  private  val jep  = Future{
 
+
+  private val jep = Future {
     val jep = new Jep(false)
     jep.runScript("./python-script/find_pic.py")
     jep
@@ -23,17 +24,16 @@ object PythonScript {
 
   def eval[T](f: Jep => T): Future[T] = jep.map(f)
 
-  def asyncFindPic(original: String, goal: String): Future[(Double, Int, Int)] = {
+
+  def findPic(original: String, goal: String, patten: String): (Double, Int, Int) = {
     val regex = "\\(([0-9|.]+), ?([0-9]+), ?([0-9]+)\\)".r
-    jep.map(jep =>
-      jep.getValue(s"jvm_find_pic('$original','$goal')") match {
+    val result = jep.map(jep =>
+      jep.getValue(s"jvm_find_pic('$original','$goal','$patten')") match {
         case regex(sim, x, y) => (sim.toDouble, x.toInt, y.toInt)
       }
     )
-  }
 
-  def findPic(original: String, goal: String): (Double, Int, Int) = {
-    Await.result(asyncFindPic(original = original, goal = goal), Duration.Inf)
+    Await.result(result, Duration.Inf)
   }
 }
 
