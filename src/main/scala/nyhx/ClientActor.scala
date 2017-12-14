14ffa50359426ac1df3ba863e9ede103ed8a8487
yujieshui <a1178615156@gmail.com>
2017-12-14 22:36:19 +0800

    [fix]   args fold error

--- src/main/scala/nyhx/ClientActor.scala
+++ src/main/scala/nyhx/ClientActor.scala
@@ -40,11 +40,11 @@ class ClientActor(args: Seq[String]) extends FSM[Status, Data] with FsmHelper[St
     )
 
     args.foldLeft(map) {
-      case (acc, "war-2-6") => map + (War -> (() => actorOf(Props(new WarAreaTwoSix(warNum)))))
-      case (acc, "war-6-4") => map + (War -> (() => actorOf(Props(new WarAreaSixActor(warNum)))))
-      case (acc, "wdj")     => map + (War -> (() => actorOf(Props(new WdjActor(warNum)))))
-      case (acc, "dismiss") => map + (Dismissed -> (() => actorOf(Props(new nyhx.fsm.DismissedActor))))
-      case _                => map
+      case (acc, "war-2-6") => acc + (War -> (() => actorOf(Props(new WarAreaTwoSix(warNum)))))
+      case (acc, "war-6-4") => acc + (War -> (() => actorOf(Props(new WarAreaSixActor(warNum)))))
+      case (acc, "wdj")     => acc + (War -> (() => actorOf(Props(new WdjActor(warNum)))))
+      case (acc, "dismiss") => acc + (Dismissed -> (() => actorOf(Props(new nyhx.fsm.DismissedActor))))
+      case (acc, _)         => acc
     }
   }
 
